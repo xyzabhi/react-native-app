742191279055512ad95d34962d6227a07b5868db
xyzabhi <abhinavkumar8083@gmail.com>
2020-10-08 04:46:54 +0530

    List added

--- App.js
+++ App.js
@@ -4,21 +4,48 @@ import {
   Button,
   StyleSheet,
   Text,
+  TextInput,
   TouchableWithoutFeedback,
   View,
+  ScrollView,
+  FlatList,
 } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("Abhinav");
+  const [people, setPeople] = useState([
+    { name: "Abhinav", age: "21", key: "1" },
+    { name: "Kumar", age: "24", key: "2" },
+    { name: "Mario", age: "21", key: "3" },
+    { name: "Bhola", age: "21", key: "4" },
+    { name: "Dhola", age: "21", key: "5" },
+    { name: "Xhola", age: "21", key: "6" },
+  ]);
+
   const clickHandler = () => {
     setName("Chotu");
   };
   return (
     <View style={styles.container}>
-      <Text>My name is {name}.</Text>
-      <View style={styles.buttonConatainer}>
-        <Button title="Update name" onPress={clickHandler} />
-      </View>
+      <FlatList
+        numColumns={3}
+        data={people}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>
+            {item.name} is {item.age} old.
+          </Text>
+        )}
+      />
+      {/* <ScrollView>
+        {people.map((item) => {
+          return (
+            <View key={item.key}>
+              <Text style={styles.item}>
+                {item.name} is {item.age} old.
+              </Text>
+            </View>
+          );
+        })}
+      </ScrollView> */}
     </View>
   );
 }
@@ -26,11 +53,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#aaa",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#eee",
+    paddingTop: 40,
+    paddingHorizontal: 20,
   },
-  buttonConatainer: {
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
+    marginHorizontal: 10,
     marginTop: 20,
   },
 });
